c41913b8553dd1ea61d47fd0ff3bd26e7ab80173
Celestia-coder <141723734+Celestia-coder@users.noreply.github.com>
2025-04-14 16:44:10 +0800

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let string = "";
+    let length = word.length;
 
+    for (let i = length - 1; i >= 0; i--) {
+        string += word[i]
+    }
+    return string;
 };
 
 // Do not edit below this line
