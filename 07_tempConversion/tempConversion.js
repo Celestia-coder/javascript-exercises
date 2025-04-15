25790b2988b1aeafc4bc65ae3fd1c77695d383cc
Celestia-coder <141723734+Celestia-coder@users.noreply.github.com>
2025-04-15 23:02:31 +0800

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // (32°F − 32) × 5/9 
+  let celcius = ((temp - 32) * 5) / 9;
+  
+  let rounded = parseFloat(celcius.toFixed(1))
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // (0°C × 9/5) + 32
+  let fahrenheit = ((temp * 9) / 5) + 32;
+  
+  let rounded = parseFloat(fahrenheit.toFixed(1))
+  return rounded;
 };
 
 // Do not edit below this line
